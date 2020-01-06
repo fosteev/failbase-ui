bf1b4a8e1363116db841730d80c0e785f6c26c86
Fost <fosteev3@gmail.com>
2020-01-07 01:49:46 +0300

    include antd

--- webpack.common.js
+++ webpack.common.js
@@ -15,6 +15,30 @@ module.exports = {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                {
+                                    useBuiltIns: "usage",
+                                    "corejs": "2",
+                                    targets: {
+                                        browsers: "> 1%, not ie 11, not op_mini all"
+                                    }
+                                }
+                            ],
+                            "@babel/preset-react"
+                        ],
+                        plugins: [
+                            ["import", { "libraryName": "antd", "libraryDirectory": "es", "style": "css" }] // `style: true` for less
+                        ]                    }
+                }
+            }
         ]
     },
     resolve: {
